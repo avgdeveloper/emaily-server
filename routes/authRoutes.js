5f0d63e9bdb3501baddfe4f8ff95bb74dd69d72b
avgdeveloper <aviad.gintzler@gmail.com>
2024-07-17 16:43:47 +0300

    google startegy with new key, change route name

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,7 +14,7 @@ module.exports = (app) => {
         res.send(req.user);
     })
 
-    app.get("/auth/current_user", (req, res) => {
+    app.get("/api/current_user", (req, res) => {
         res.send(req.session);
     });
 }
