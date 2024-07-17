5dba149c408cdb31355b77aab26e5bb6df0302bb
avgdeveloper <aviad.gintzler@gmail.com>
2024-07-17 15:07:57 +0300

    fixes

--- index.js
+++ index.js
@@ -12,10 +12,17 @@ const app = express();
 
 app.use(
     cookieSession({
+        name: "local",
         maxAge: 30 * 24 * 60 * 60 * 1000,
         keys: [keys.cookieKey]
     })
 );
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 app.use(passport.initialize());
 app.use(passport.session());
 
